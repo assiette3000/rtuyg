0b4fbbeeb2d465638a157b6d99b634fb2baba40f
plts <augustusplatypus@gmail.com>
2025-09-09 20:17:17 +0200

    add camera, fix some stuff

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,8 @@ int main(int argc, char *argv[])
     double character_x = 48., character_y = 64.;
     double character_vx = 0., character_vy = 0.;
 
+    double camera_x = 0., camera_y = 0.;
+
     bool hook_shooting = false;
     bool hook_attached = false;
     double hook_length = 0.;
@@ -115,26 +117,28 @@ int main(int argc, char *argv[])
 
         if (hook_shooting) {
             hook_length += 8.;
+            if (hook_length >= 8.*32.) {
+                hook_shooting = false;
+            }
         }
 
         if (hook_attached) {
             if (hook_length >= 4.*32.) {
-                hook_length -= 4.;
-                character_vx = 4.;
-                character_vy = -4.;
+                hook_length -= 8.;
             }
             else {
                 hook_angle -= .1;
                 if (hook_angle <= -(M_PI*3./4.)) {
                     hook_attached = false;
                 }
-                double old_x = character_x;
-                double old_y = character_y;
-                character_x = hook_end_x - hook_length * std::cos(hook_angle) - 32.;
-                character_y = hook_end_y - hook_length * std::sin(hook_angle);
-                character_vx = character_x - old_x;
-                character_vy = character_y - old_y;
             }
+            
+            double old_x = character_x;
+            double old_y = character_y;
+            character_x = hook_end_x - hook_length * std::cos(hook_angle) - 32.;
+            character_y = hook_end_y - hook_length * std::sin(hook_angle);
+            character_vx = character_x - old_x;
+            character_vy = character_y - old_y;
         }
         else {
             character_vy += 9.81/32.;
@@ -174,28 +178,39 @@ int main(int argc, char *argv[])
             hook_attached = true;
         }
 
+
+        // render
+
         SDL_SetRenderDrawColor(renderer, 0, 128, 255, 255);
         SDL_RenderClear(renderer);
+
+        camera_x = character_x + 16. - 320.;
+        camera_y = character_y + 16. - 240.;
 
         SDL_FRect box_rect = {0, 0, 32, 32};
         for (int y = 0; y < level.height; y++) {
             for (int x = 0; x < level.width; x++) {
                 if (level.data[x+y*level.width]) {
-                    box_rect.x = x*32.;
-                    box_rect.y = y*32.;
+                    box_rect.x = x*32. - camera_x;
+                    box_rect.y = y*32. - camera_y;
                     SDL_RenderTexture(renderer, texture_box, nullptr, &box_rect);
                 }
             }
         }
 
-        character_rect.x = character_x;
-        character_rect.y = character_y;
+        character_rect.x = character_x - camera_x;
+        character_rect.y = character_y - camera_y;
 
         SDL_RenderTexture(renderer, texture_character, nullptr, &character_rect);
 
         if (hook_shooting || hook_attached) {
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-            SDL_RenderLine(renderer, character_x+32., character_y, hook_end_x, hook_end_y);
+            SDL_RenderLine(renderer,
+                character_x + 32. - camera_x,
+                character_y - camera_y,
+                hook_end_x - camera_x,
+                hook_end_y - camera_y
+            );
         }
 
         SDL_RenderPresent(renderer);
